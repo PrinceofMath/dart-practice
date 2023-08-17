11378acca69190359bd5939505a9062e2daa7677
Jamoliddin Kodirov <princeofmath13@gmail.com>
2023-08-17 13:45:37 +0500

    operators

---
+++ operators/bin/example1.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  const int age1 = 64;
+  const int age2 = 30;
+
+  print(age1 + age2);
+
+  print(age1 * age2);
+
+  const divided = age1 / age2;
+
+  print(divided);
+
+  const intDivided = age1 ~/ age2;
+
+  print(intDivided);
+}
---
+++ operators/bin/example2.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  int age = 30;
+
+  int age2 = --age;
+
+  print(age);
+  print(age2);
+
+  bool isActive = false;
+  print(!isActive);
+  print(isActive);
+}
